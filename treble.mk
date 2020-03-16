a1e2ca3b9a09185546d84e2b6eb6c85ce34c1eb9
Viciouss <xviciouss@gmail.com>
2020-03-16 23:57:30 +0100

    added graphics mapper service

--- treble.mk
+++ treble.mk
@@ -1,6 +1,7 @@
 # Graphics HAL
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.0-service \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
