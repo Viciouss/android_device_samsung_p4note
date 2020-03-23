af26a3a4a350a92ceae9fca07c5b22f661cde8b4
Viciouss <xviciouss@gmail.com>
2020-03-23 20:30:09 +0100

    added thermal hals

--- treble.mk
+++ treble.mk
@@ -41,6 +41,12 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service \
 
+# Thermal HAL
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.default
+
 # Light HAL
 # PRODUCT_PACKAGES += \
 #    android.hardware.light@2.0-impl \
