1ecdccc061873fab941b99272ed41f08aaacdbbe
Martin Jücker <martin.juecker@gmail.com>
2021-05-02 22:26:35 +0200

    HWC: remove client backend override
    there have been some adjustments to transparency in the kernel, there
    are still some occasional flashes, but it's mostly ok and usable
    
    Signed-off-by: Martin Jücker <martin.juecker@gmail.com>

--- mesa.mk
+++ mesa.mk
@@ -9,6 +9,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=131072 \
     ro.hardware.gralloc=gbm \
     ro.hardware.hwcomposer=drm \
-
-# use GPU composer because of screen flickering
-PRODUCT_PROPERTY_OVERRIDES += vendor.hwc.backend_override=client
