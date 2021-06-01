10af99e96af4c21ce00105ef552e1acf6a3d1d88
Martin Jücker <martin.juecker@gmail.com>
2021-06-01 11:00:47 +0200

    remove temporary scripts that were used for initial boot
    Signed-off-by: Martin Jücker <martin.juecker@gmail.com>

--- temporary/temporary.mk
+++
@@ -1,4 +0,0 @@
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/console.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/console.rc \
-    $(LOCAL_PATH)/logger.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/logger.rc \
-    $(LOCAL_PATH)/logger.sh:$(TARGET_COPY_OUT_VENDOR)/bin/logger.sh \
