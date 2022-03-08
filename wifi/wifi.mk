79b105367c903d4ac32705276bf3b397e42eeb5d
Martin Jücker <martin.juecker@gmail.com>
2022-03-08 20:40:02 +0100

    hal: wifi: add wpa_supplicant.conf and regulatory.db packages
    Signed-off-by: Martin Jücker <martin.juecker@gmail.com>

--- wifi/wifi.mk
+++ wifi/wifi.mk
@@ -2,12 +2,18 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/brcmfmac4334-sdio.bin:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/brcm/brcmfmac4334-sdio.bin \
     $(LOCAL_PATH)/brcmfmac4334-sdio.samsung,n8010.txt:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/brcm/brcmfmac4334-sdio.samsung,n8010.txt \
-    $(LOCAL_PATH)/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/init.wifi.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.wifi.rc
 
-PRODUCT_PACKAGES += libwpa_client wpa_supplicant hostapd wificond
+PRODUCT_PACKAGES += \
+    libwpa_client \
+    wpa_supplicant \
+    wpa_supplicant.conf \
+    hostapd \
+    wificond \
+    regulatory.db \
+    regulatory.db.p7s \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0 \
