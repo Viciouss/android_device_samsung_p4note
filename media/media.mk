c5690c485517dd110831028e678aa5dfcc0359ff
Martin Jücker <martin.juecker@gmail.com>
2022-05-17 17:12:50 +0200

    roll back some media changes for now
    Signed-off-by: Martin Jücker <martin.juecker@gmail.com>

--- media/media.mk
+++ media/media.mk
@@ -1,8 +1,5 @@
 PRODUCT_PACKAGES += \
-    android.hardware.media.c2@1.0-service-v4l2
-
-PRODUCT_PACKAGES += \
-    libv4l2_codec2_components \
+    android.hardware.media.c2@1.0-service
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
@@ -12,13 +9,10 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy \
-    $(LOCAL_PATH)/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
+    $(LOCAL_PATH)/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/s5p-mfc.fw:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/s5p-mfc.fw \
-
-DEVICE_MANIFEST_FILE += $(LOCAL_PATH)/manifest.xml
 
 # Create input surface on the framework side
 PRODUCT_VENDOR_PROPERTIES += \
