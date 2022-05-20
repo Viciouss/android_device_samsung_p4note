6baa20d2950b609885441267724d50e8fba2637c
Martin Jücker <martin.juecker@gmail.com>
2022-05-20 18:46:03 +0200

    WIFI: add  wpa_supplicant.conf
    Signed-off-by: Martin Jücker <martin.juecker@gmail.com>

--- wifi/wifi.mk
+++ wifi/wifi.mk
@@ -4,6 +4,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/brcmfmac4334-sdio.samsung,n8010.txt:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/brcm/brcmfmac4334-sdio.samsung,n8010.txt \
     $(LOCAL_PATH)/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/init.wifi.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.wifi.rc
 
 PRODUCT_PACKAGES += \
