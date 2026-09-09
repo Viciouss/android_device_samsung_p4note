ada0e70a38cdc102ff2a5533c58db641bd6920ad
Martin Juecker <martin.juecker@gmail.com>
2026-09-09 10:47:31 +0200

    fix: add missing wifi config for n8010

--- n8010/n8010.mk
+++ n8010/n8010.mk
@@ -3,3 +3,6 @@ $(call inherit-product, $(LOCAL_PATH)/../device-common.mk)
 PRODUCT_NAME := n8010
 PRODUCT_DEVICE := n8010
 PRODUCT_MODEL := GT-N8010
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/../wifi/brcmfmac4334-sdio-n8010.txt:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/brcm/brcmfmac4334-sdio.txt
